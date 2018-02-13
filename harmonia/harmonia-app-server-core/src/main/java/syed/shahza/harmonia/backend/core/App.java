da0060751d5a9064de843cfec1fd079efa5e908d
Shahza Syed <ll13s6s@leeds.ac.uk>
2018-02-13 15:25:55 +0000

    HAR-1: Added remaining backend maven modules

---
+++ harmonia/harmonia-app-server-core/src/main/java/syed/shahza/harmonia/backend/core/App.java
@@ -0,0 +1,13 @@
+package syed.shahza.harmonia.backend.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

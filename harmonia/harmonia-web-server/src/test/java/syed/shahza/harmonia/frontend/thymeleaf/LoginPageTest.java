0d81e751f5b300d6f8ddc190c30c60dc0b58427d
Shahza Syed <ll13s6s@leeds.ac.uk>
2018-02-19 20:14:32 +0000

    HAR-3: Edited login.html by adding heading & labels + tests

--- harmonia/harmonia-web-server/src/test/java/syed/shahza/harmonia/frontend/thymeleaf/LoginPageTest.java
+++ harmonia/harmonia-web-server/src/test/java/syed/shahza/harmonia/frontend/thymeleaf/LoginPageTest.java
@@ -21,6 +21,22 @@ public class LoginPageTest extends ThymeleafTemplateTest {
     public void setUp() {
         this.tags = process();
     }
+    
+    @Test
+    public void onlyThreeInputFieldsShouldExist() throws NodeSelectorException {
+    	//2 input fields + button (counts as input)
+    	assertThat(this.tags.matching("input").size(), is(3));
+    }
+    
+    @Test
+    public void usernameLabelShouldExist() throws NodeSelectorException {
+    	assertThat(this.tags.matching("label").get(0).text(), is("Username"));
+    }
+    
+    @Test
+    public void passwordLabelShouldExist() throws NodeSelectorException {
+    	assertThat(this.tags.matching("label").get(1).text(), is("Password"));
+    }
 
     @Test
     public void loginButtonShouldExist() throws NodeSelectorException {

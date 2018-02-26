a04ff31ca2a3b0af034d81ef517cbe268778737c
Shahza Syed <ll13s6s@leeds.ac.uk>
2018-02-26 22:09:07 +0000

    HAR-9: Added lecture service

---
+++ harmonia/harmonia-app-server-core/src/main/java/syed/shahza/harmonia/backend/core/service/LectureService.java
@@ -0,0 +1,16 @@
+package syed.shahza.harmonia.backend.core.service;
+
+import syed.shahza.harmonia.backend.core.domain.Lecture;
+import syed.shahza.harmonia.backend.core.repository.LectureRepository;
+
+public class LectureService {
+	private final LectureRepository lectureRepository;
+	
+	public LectureService(LectureRepository lectureRepository) {
+		this.lectureRepository = lectureRepository;
+	}
+
+	public Lecture create(Lecture lecture) {
+		return lectureRepository.create(lecture);
+	}
+}

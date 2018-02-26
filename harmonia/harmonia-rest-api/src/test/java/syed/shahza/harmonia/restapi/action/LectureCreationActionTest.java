d256b327659c2a05102714b253fb0820355a751b
Shahza Syed <ll13s6s@leeds.ac.uk>
2018-02-26 18:46:46 +0000

    HAR-8: Added rest api lecture creation action

---
+++ harmonia/harmonia-rest-api/src/test/java/syed/shahza/harmonia/restapi/action/LectureCreationActionTest.java
@@ -0,0 +1,33 @@
+package syed.shahza.harmonia.restapi.action;
+
+import static org.mockito.Mockito.verify;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import syed.shahza.harmonia.backend.dto.LectureDto;
+import syed.shahza.harmonia.restapi.client.RestClient;
+
+@RunWith(MockitoJUnitRunner.class)
+public class LectureCreationActionTest {
+    private LectureCreationAction lectureCreationAction;
+
+    @Mock
+    private RestClient mockRestClient;
+
+    @Before
+    public void before() {
+        this.lectureCreationAction = new LectureCreationAction(this.mockRestClient);
+    }
+
+    @Test
+    public void lectureCreateWillInvokeMockRestClientGetMethodWithCorrectParameters() {
+    	LectureDto lectureDto = new LectureDto();
+        this.lectureCreationAction.create(lectureDto);
+
+        verify(this.mockRestClient).post("/lecture/create", lectureDto, LectureDto.class);
+    }
+}

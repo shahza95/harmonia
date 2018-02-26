686627a64ab7212ad5576c7504581bc47b0009c9
Shahza Syed <ll13s6s@leeds.ac.uk>
2018-02-26 22:10:25 +0000

    HAR-9: Added core configuration beans of lecture service and repo

--- harmonia/harmonia-app-server-core/src/main/java/syed/shahza/harmonia/backend/core/configuration/CoreConfiguration.java
+++ harmonia/harmonia-app-server-core/src/main/java/syed/shahza/harmonia/backend/core/configuration/CoreConfiguration.java
@@ -3,7 +3,9 @@ package syed.shahza.harmonia.backend.core.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import syed.shahza.harmonia.backend.core.repository.LectureRepository;
 import syed.shahza.harmonia.backend.core.repository.LecturerRepository;
+import syed.shahza.harmonia.backend.core.service.LectureService;
 import syed.shahza.harmonia.backend.core.service.LecturerService;
 
 @Configuration
@@ -13,9 +15,19 @@ public class CoreConfiguration {
 	public LecturerRepository lecturerRepository() {
 		return new LecturerRepository();
 	}
+	
+	@Bean
+	public LectureRepository lectureRepository() {
+		return new LectureRepository();
+	}
 
 	@Bean
     public LecturerService lecturerService() {
         return new LecturerService(lecturerRepository());
     }
+	
+	@Bean
+	public LectureService lectureService() {
+		return new LectureService(lectureRepository());
+	}
 }

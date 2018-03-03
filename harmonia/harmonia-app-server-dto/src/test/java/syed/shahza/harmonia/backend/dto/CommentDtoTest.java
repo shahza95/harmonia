073e5b593b5893dbcd57d2de4c03c09715abac17
Shahza Syed <ll13s6s@leeds.ac.uk>
2018-03-03 16:34:55 +0000

    HAR-20: Added comment dto object

---
+++ harmonia/harmonia-app-server-dto/src/test/java/syed/shahza/harmonia/backend/dto/CommentDtoTest.java
@@ -0,0 +1,18 @@
+package syed.shahza.harmonia.backend.dto;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static syed.shahza.harmonia.backend.dto.TestCommentDtos.aValidCommentDto;
+
+import org.junit.Test;
+
+public class CommentDtoTest {
+    @Test
+    public void canRetrieveCorrectMessageOnceSet() {
+    	CommentDto commentDto = aValidCommentDto().build();
+        String randomMessageString = "another message";
+        commentDto.setMessage(randomMessageString);
+
+        assertThat(commentDto.getMessage(), is(randomMessageString));
+    }
+}

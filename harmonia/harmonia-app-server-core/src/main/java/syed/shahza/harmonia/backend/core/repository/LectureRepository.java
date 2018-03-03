0e84f9562a685c7030674171ce59759eeaee9338
Shahza Syed <ll13s6s@leeds.ac.uk>
2018-03-03 19:08:06 +0000

    HAR-20: Added comments (add, get) to lecture repository

--- harmonia/harmonia-app-server-core/src/main/java/syed/shahza/harmonia/backend/core/repository/LectureRepository.java
+++ harmonia/harmonia-app-server-core/src/main/java/syed/shahza/harmonia/backend/core/repository/LectureRepository.java
@@ -3,29 +3,46 @@ package syed.shahza.harmonia.backend.core.repository;
 import static syed.shahza.harmonia.backend.core.domain.Lecture.aLecture;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import syed.shahza.harmonia.backend.core.domain.Comment;
+import syed.shahza.harmonia.backend.core.domain.Comments;
 import syed.shahza.harmonia.backend.core.domain.Lecture;
 
 public class LectureRepository {
-	private ArrayList<Lecture> lectures = new ArrayList<Lecture>();
-	
+	private List<Lecture> lectures = new ArrayList<>();
+	private Map<String, ArrayList<Comment>> comments = new HashMap<String, ArrayList<Comment>>();
+
 	public Lecture create(Lecture lecture) {
 		lectures.add(lecture);
 		return lecture;
 	}
-	
+
 	public Lecture retrieveLectureFromPassword(String password) {
-		for(Lecture lecture: lectures) {
-			if(lecture.getPassword().equals(password)) {
+		for (Lecture lecture : lectures) {
+			if (lecture.getPassword().equals(password)) {
 				return lecture;
 			}
 		}
 		return aLecture().build();
 	}
-	
-//	public CommentList
-//	
-//	public void addComment(String lectureTitle, CommentDto commentDto) {
-//		
-//	}
+
+	public Comments getComments(String lectureTitle) {
+		return Comments.aCommentListBuilder().commentList(comments.get(lectureTitle)).build();
+	}
+
+	public Comment addComment(String lectureTitle, Comment comment) {
+		ArrayList<Comment> commentList;
+		if(comments.containsKey(comment)) {
+			commentList = comments.get(lectureTitle);
+		} else {
+			commentList = new ArrayList<Comment>();
+		}
+		
+		commentList.add(comment);
+		comments.put(lectureTitle, commentList);
+		return comment;
+	}
 }
--- harmonia/harmonia-app-server-core/src/test/java/syed/shahza/harmonia/backend/core/repository/LectureRepositoryTest.java
+++ harmonia/harmonia-app-server-core/src/test/java/syed/shahza/harmonia/backend/core/repository/LectureRepositoryTest.java
@@ -1,32 +1,41 @@
 package syed.shahza.harmonia.backend.core.repository;
 
+import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 import static syed.shahza.harmonia.backend.core.domain.TestLecture.aValidLecture;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import syed.shahza.harmonia.backend.core.domain.Comment;
+import syed.shahza.harmonia.backend.core.domain.Comments;
 import syed.shahza.harmonia.backend.core.domain.Lecture;
+import syed.shahza.harmonia.backend.core.domain.TestComment;
 
 @RunWith(MockitoJUnitRunner.class)
 public class LectureRepositoryTest {
 	private LectureRepository lectureRepository;
+	private Lecture lecture;
+	private Comment comment;
+	
+	@Before
+	public void before() {
+		this.lectureRepository = new LectureRepository();
+		this.lecture = aValidLecture().build();
+		this.comment = TestComment.aValidComment().build();
+	}
 	
     @Test
     public void createReturnsAddedLecture() {
-    	this.lectureRepository = new LectureRepository();
-        Lecture lecture = aValidLecture().build();
-
-        assertThat(lectureRepository.create(lecture), is(lecture));
+        assertThat(this.lectureRepository.create(lecture), is(lecture));
     }
     
     @Test
     public void joinReturnsLectureObjectIfPasswordValid() {
-    	Lecture lecture = aValidLecture().build();
-    	this.lectureRepository = new LectureRepository();
     	this.lectureRepository.create(lecture);
     	
     	assertThat(lectureRepository.retrieveLectureFromPassword(lecture.getPassword()), is(lecture));
@@ -34,8 +43,27 @@ public class LectureRepositoryTest {
     
     @Test
     public void joinReturnsEmptyLectureObjectIfPasswordInvalid() {
-    	this.lectureRepository = new LectureRepository();
     	Lecture lecture = lectureRepository.retrieveLectureFromPassword("passwordForNonExistentLecture");
     	assertTrue(lecture.isEmpty());
+    }
+    
+    @Test
+    public void addCommentShouldReturnTheComment() {
+    	assertThat(this.lectureRepository.addComment("someTitle", comment), is(comment));
+    }
+    
+    @Test
+    public void getCommentsShouldReturnAllCommentsForParticularLecture() {
+    	this.lectureRepository.addComment(lecture.getTitle(), comment);
+    	this.lectureRepository.addComment(lecture.getTitle(), TestComment.aValidComment().message("no").build());
+    	
+    	assertThat(this.lectureRepository.getComments(lecture.getTitle()).getCommentList().size(), is(1));
+    }
+    
+    @Test
+    public void getCommentsShouldReturnCommentsObject() {
+    	this.lectureRepository.addComment(lecture.getTitle(), comment);
+    	
+    	assertThat(this.lectureRepository.getComments(lecture.getTitle()), instanceOf(Comments.class));
     }
 }

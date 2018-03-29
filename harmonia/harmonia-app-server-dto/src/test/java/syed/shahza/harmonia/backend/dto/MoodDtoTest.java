44e8b553be3975dc173143a22b0ba46d43f23fdb
Shahza Syed <ll13s6s@leeds.ac.uk>
2018-03-29 12:01:41 +0100

    HAR-35: Added emotion dto to mooddto

---
+++ harmonia/harmonia-app-server-dto/src/test/java/syed/shahza/harmonia/backend/dto/MoodDtoTest.java
@@ -0,0 +1,41 @@
+package syed.shahza.harmonia.backend.dto;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static syed.shahza.harmonia.backend.dto.TestMoodDto.aValidMoodDto;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MoodDtoTest {
+	private MoodDto moodDto;
+	
+	@Before
+	public void before() {
+		moodDto = aValidMoodDto().build();
+	}
+
+	@Test
+    public void canRetrieveCorrectEmojiOnceSet() {
+        String randomEmojiString = "&#x1F642;";
+        moodDto.setEmoji(randomEmojiString);
+
+        assertThat(moodDto.getEmoji(), is(randomEmojiString));
+    }
+	
+	@Test
+	public void canRetrieveCorrectEmotionDtoOnceSet() {
+		EmotionDto emotionDto = EmotionDto.HAPPY;
+		moodDto.setEmotionDto(emotionDto);
+		
+		assertThat(moodDto.getEmotionDto(), is(emotionDto));
+	}
+
+    @Test
+    public void canRetrieveCorrectLectureDtoOnceSet() {
+    	LectureDto lectureDto = TestLectureDto.aValidLectureDto().build();
+    	moodDto.setLectureDto(lectureDto);
+    	
+    	assertThat(moodDto.getLectureDto(), is(lectureDto));
+    }
+}

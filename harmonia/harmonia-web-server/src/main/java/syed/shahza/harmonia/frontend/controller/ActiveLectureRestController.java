2644f0b520be1d95109d755788d3df729fa81b48
Shahza Syed <ll13s6s@leeds.ac.uk>
2018-03-30 12:09:39 +0100

    HAR-36: Mood chart only now updates every 5s

--- harmonia/harmonia-web-server/src/main/java/syed/shahza/harmonia/frontend/controller/ActiveLectureRestController.java
+++ harmonia/harmonia-web-server/src/main/java/syed/shahza/harmonia/frontend/controller/ActiveLectureRestController.java
@@ -1,5 +1,9 @@
 package syed.shahza.harmonia.frontend.controller;
 
+import static syed.shahza.harmonia.frontend.controller.ActiveLectureControllerLecturer.getMoodSummaryMap;
+
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,18 +11,26 @@ import org.springframework.web.bind.annotation.RestController;
 
 import syed.shahza.harmonia.backend.dto.CommentDtoList;
 import syed.shahza.harmonia.restapi.action.GetAllCommentsAction;
+import syed.shahza.harmonia.restapi.action.GetAllMoodsAction;
 
 @RestController
 @RequestMapping("**/lecture")
 public class ActiveLectureRestController {
 	private final GetAllCommentsAction getAllCommentsAction;
+	private final GetAllMoodsAction getAllMoodsAction;
 
-	public ActiveLectureRestController(GetAllCommentsAction getAllCommentsAction) {
+	public ActiveLectureRestController(GetAllCommentsAction getAllCommentsAction, GetAllMoodsAction getAllMoodsAction) {
 		this.getAllCommentsAction = getAllCommentsAction;
+		this.getAllMoodsAction = getAllMoodsAction;
 	}
 	
     @RequestMapping(value = "/active/{lectureTitle}/comments/json", method = RequestMethod.GET)
     public CommentDtoList getAllComments(@PathVariable String lectureTitle) {
         return this.getAllCommentsAction.getAll(lectureTitle);
+    }
+    
+    @RequestMapping(value = "/active/{lectureTitle}/mood/json", method = RequestMethod.GET)
+    public Map<String, Integer> getMoodSummary(@PathVariable String lectureTitle) {
+    	return getMoodSummaryMap(this.getAllMoodsAction.getAll(lectureTitle));
     }
 }
--- harmonia/harmonia-web-server/src/test/java/syed/shahza/harmonia/frontend/controller/ActiveLectureRestControllerTest.java
+++ harmonia/harmonia-web-server/src/test/java/syed/shahza/harmonia/frontend/controller/ActiveLectureRestControllerTest.java
@@ -4,6 +4,9 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.when;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,7 +16,9 @@ import org.mockito.runners.MockitoJUnitRunner;
 
 import syed.shahza.harmonia.backend.dto.CommentDtoList;
 import syed.shahza.harmonia.backend.dto.TestCommentDtoList;
+import syed.shahza.harmonia.backend.dto.TestMoodDtoList;
 import syed.shahza.harmonia.restapi.action.GetAllCommentsAction;
+import syed.shahza.harmonia.restapi.action.GetAllMoodsAction;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ActiveLectureRestControllerTest {
@@ -22,12 +27,15 @@ public class ActiveLectureRestControllerTest {
     
     @Mock
     private GetAllCommentsAction mockGetAllCommentsAction;
+    
+    @Mock
+    private GetAllMoodsAction mockGetAllMoodsAction;
 
     
     @Before
     public void before() {
     	this.lectureTitle = "someTitle";
-        this.lectureController = new ActiveLectureRestController(this.mockGetAllCommentsAction);
+        this.lectureController = new ActiveLectureRestController(this.mockGetAllCommentsAction, this.mockGetAllMoodsAction);
     }
     
     @Test
@@ -43,5 +51,25 @@ public class ActiveLectureRestControllerTest {
     	when(this.mockGetAllCommentsAction.getAll(lectureTitle)).thenReturn(commentDtoList);
     
     	assertThat(this.lectureController.getAllComments(this.lectureTitle), is(commentDtoList));
+    }
+    
+    @Test
+    public void getMoodSummaryInvokesGetAllMoodsAction() {
+    	when(this.mockGetAllMoodsAction.getAll(lectureTitle)).thenReturn(TestMoodDtoList.aFilledMoodDtoList(2));
+    	this.lectureController.getMoodSummary(this.lectureTitle);
+    	
+    	Mockito.verify(this.mockGetAllMoodsAction).getAll(this.lectureTitle);
+    }
+    
+    @Test
+    public void getMoodSummaryReturnsACommentDtoList() {
+    	int numberOfSameMood = 2;
+    	when(this.mockGetAllMoodsAction.getAll(lectureTitle)).thenReturn(TestMoodDtoList.aFilledMoodDtoList(numberOfSameMood));
+    	Map<String, Integer> moodMap = new HashMap<String, Integer>();
+    	moodMap.put("HAPPY", numberOfSameMood);
+    	moodMap.put("SAD", 0);
+    	moodMap.put("CONFUSED", 0);
+    	
+    	assertThat(this.lectureController.getMoodSummary(this.lectureTitle), is(moodMap));
     }
 }

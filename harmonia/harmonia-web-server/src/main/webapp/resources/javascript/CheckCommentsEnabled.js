ac6f0bb93526ca35935a9f2962764762ad24e651
Shahza Syed <ll13s6s@leeds.ac.uk>
2018-03-31 15:56:06 +0100

    HAR-39: Fixed student side toggle delay between refresh & screen change

--- harmonia/harmonia-web-server/src/main/webapp/resources/javascript/CheckCommentsEnabled.js
+++ harmonia/harmonia-web-server/src/main/webapp/resources/javascript/CheckCommentsEnabled.js
@@ -6,15 +6,20 @@ function setGetLectureUrl(getUrl) {
 }
 
 function checkEnabled() {
+	enableDisable();
 	setInterval(function(){
-			$.getJSON(urls.getLectureUrl, function(data) {
-				if(data.commentsEnabled) {
-					$('.btn').prop('disabled', false);
-					$('#Comment').prop('disabled', false);
-				} else {
-					$('.btn').prop('disabled', true);
-					$('#Comment').prop('disabled', true);
-				}
-			});
+		enableDisable();
 	}, 5000);
+}
+
+function enableDisable() {
+	$.getJSON(urls.getLectureUrl, function(data) {
+		if(data.commentsEnabled) {
+			$('.btn').prop('disabled', false);
+			$('#Comment').prop('disabled', false);
+		} else {
+			$('.btn').prop('disabled', true);
+			$('#Comment').prop('disabled', true);
+		}
+	});
 }

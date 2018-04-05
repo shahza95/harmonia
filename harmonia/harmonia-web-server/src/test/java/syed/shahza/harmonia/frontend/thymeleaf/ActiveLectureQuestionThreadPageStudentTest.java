31b188186300b22e8b50acde6701673f21086aa5
Shahza Syed <ll13s6s@leeds.ac.uk>
2018-04-05 21:53:11 +0100

    HAR-52: Added missing thymeleaf tests

--- harmonia/harmonia-web-server/src/test/java/syed/shahza/harmonia/frontend/thymeleaf/ActiveLectureQuestionThreadPageStudentTest.java
+++ harmonia/harmonia-web-server/src/test/java/syed/shahza/harmonia/frontend/thymeleaf/ActiveLectureQuestionThreadPageStudentTest.java
@@ -22,6 +22,7 @@ public class ActiveLectureQuestionThreadPageStudentTest extends ThymeleafTemplat
     private HtmlElements tags;
     private LectureDto lectureDto;
     private QuestionDto questionDto;
+    private Map<String, Object> model;
     
     public ActiveLectureQuestionThreadPageStudentTest() {
         super("/student/activeLectureQuestionThread");
@@ -29,11 +30,11 @@ public class ActiveLectureQuestionThreadPageStudentTest extends ThymeleafTemplat
     
     @Before
     public void setUp() {
-        Map<String, Object> model = new HashMap<>();
+        this.model = new HashMap<>();
         this.lectureDto = TestLectureDto.anActiveLectureDto().build();
         this.questionDto = TestQuestionDto.aValidQuestionDto().build();
-        model.put("lectureDto", this.lectureDto);
-        model.put("questionDto", this.questionDto);
+        this.model.put("lectureDto", this.lectureDto);
+        this.model.put("questionDto", this.questionDto);
         this.tags = process(model);
     }
     
@@ -50,5 +51,18 @@ public class ActiveLectureQuestionThreadPageStudentTest extends ThymeleafTemplat
     @Test
     public void correctModelShouldFillQuestion() throws NodeSelectorException {
     	assertThat(this.tags.matching("p").get(0).text(), is(questionDto.getQuestion()));
+    }
+    
+    @Test
+    public void correctModelShouldFillAnswer() throws NodeSelectorException {
+    	assertThat(this.tags.matching("p").get(1).text(), is(questionDto.getAnswer()));
+    }
+    
+    @Test
+    public void noAnswerDivIfThereIsNoAnswer() throws NodeSelectorException {
+    	questionDto.setAnswer(null);
+    	this.model.put("questionDto", questionDto);
+    	this.tags = process(model);
+    	assertThat(this.tags.matching("p").size(), is(1));
     }
 }

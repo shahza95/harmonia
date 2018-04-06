94bec58e52f8edf295e808fa64ee4e1590b1d685
Shahza Syed <ll13s6s@leeds.ac.uk>
2018-04-06 20:17:44 +0100

    Both front and back ends are ready to be deployed onto AWS. Run mvn install in app-server and web-server to generate new WAR files (see **/target/ directory)

---
+++ harmonia/harmonia-app-server/src/main/java/syed/shahza/harmonia/backend/ServletInitializer.java
@@ -0,0 +1,11 @@
+package syed.shahza.harmonia.backend;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(HarmoniaApp.class);
+	}
+}
---
+++ harmonia/harmonia-web-server/src/main/java/syed/shahza/harmonia/frontend/ServletInitializer.java
@@ -0,0 +1,11 @@
+package syed.shahza.harmonia.frontend;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(HarmoniaWebApp.class);
+	}
+}

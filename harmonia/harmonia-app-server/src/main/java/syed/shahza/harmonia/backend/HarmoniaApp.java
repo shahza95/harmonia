84b311f1e1dcd80ba04ea2a5b7cab9b4608cae68
Shahza Syed <ll13s6s@leeds.ac.uk>
2018-04-24 01:58:06 +0100

    HAR-60: Added comments to app-server maven module

--- harmonia/harmonia-app-server/src/main/java/syed/shahza/harmonia/backend/HarmoniaApp.java
+++ harmonia/harmonia-app-server/src/main/java/syed/shahza/harmonia/backend/HarmoniaApp.java
@@ -3,6 +3,7 @@ package syed.shahza.harmonia.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Back end spring boot application launching
 @SpringBootApplication
 public class HarmoniaApp {
 
